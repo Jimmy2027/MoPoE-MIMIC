ca08e1409097337f1e2397a813f782b7a06ccb2f
Thomas Sutter <t.sutter@me.com>
2020-09-14 11:40:09 +0200

    remove commented lines in mst mm_vae (not needed anymore after intro of BaseClass)

--- mnistsvhntext/networks/VAEtrimodalSVHNMNIST.py
+++ mnistsvhntext/networks/VAEtrimodalSVHNMNIST.py
@@ -31,8 +31,6 @@ class VAEtrimodalSVHNMNIST(BaseMMVae, nn.Module):
         self.decoder_m3 = self.decoder_m3.to(flags.device);
 
 
-
-
     def forward(self, input_batch):
         latents = self.inference(input_batch);
         results = dict();
@@ -76,76 +74,6 @@ class VAEtrimodalSVHNMNIST(BaseMMVae, nn.Module):
         return results;
 
 
-    #def divergence_static_prior(self, mus, logvars, weights=None):
-    #    if weights is None:
-    #        weights=self.weights;
-    #    weights = weights.clone();
-    #    weights[0] = 0.0;
-    #    weights = utils.reweight_weights(weights);
-    #    div_measures = calc_group_divergence_moe(self.flags,
-    #                                             mus,
-    #                                             logvars,
-    #                                             weights,
-    #                                             normalization=self.flags.batch_size);
-    #    divs = dict();
-    #    divs['joint_divergence'] = div_measures[0];
-    #    divs['individual_divs'] = div_measures[1];
-    #    divs['dyn_prior'] = None;
-    #    return divs;
-
-
-    #def divergence_dynamic_prior(self, mus, logvars, weights=None):
-    #    if weights is None:
-    #        weights = self.weights_jsd;
-    #    div_measures = calc_alphaJSD_modalities(self.flags,
-    #                                            mus,
-    #                                            logvars,
-    #                                            weights,
-    #                                            normalization=self.flags.batch_size);
-    #    divs = dict();
-    #    divs['joint_divergence'] = div_measures[0];
-    #    divs['individual_divs'] = div_measures[1];
-    #    divs['dyn_prior'] = div_measures[2];
-    #    return divs;
-
-
-
-    #def moe_fusion(self, mus, logvars, weights=None):
-    #    if weights is None:
-    #        weights = self.weights;
-    #    weights[0] = 0.0;
-    #    weights = utils.reweight_weights(weights);
-    #    num_samples = mus[0].shape[0];
-    #    mu_moe, logvar_moe = utils.mixture_component_selection(self.flags,
-    #                                                           mus,
-    #                                                           logvars,
-    #                                                           weights);
-    #    return [mu_moe, logvar_moe];
-
-
-    #def poe_fusion(self, mus, logvars, weights=None):
-    #    mu_poe, logvar_poe = poe(mus, logvars);
-    #    return [mu_poe, logvar_poe];
-
-
-    #def fusion_condition_moe(self, subset):
-    #    if len(subset) == 1:
-    #        return True;
-    #    else:
-    #        return False;
-
-
-    #def fusion_condition_poe(self, subset):
-    #    if len(subset) == len(self.modalities):
-    #        return True;
-    #    else:
-    #        return False;
-
-
-    #def fusion_condition_joint(self, subset):
-    #    return True;
-
-
     def encode(self, input_batch):
         latents = dict();
         if 'mnist' in input_batch.keys():
@@ -173,53 +101,6 @@ class VAEtrimodalSVHNMNIST(BaseMMVae, nn.Module):
             latents['text_style'] = [None, None];
             latents['text'] = [None, None];
         return latents;
-
-
-    #def inference(self, input_batch, num_samples=None):
-    #    if num_samples is None:
-    #        num_samples = self.flags.batch_size;
-    #    latents = dict();
-    #    enc_mods = self.encode(input_batch);
-    #    latents['modalities'] = enc_mods;
-    #    mus = [torch.zeros(1, num_samples,
-    #                       self.flags.class_dim).to(self.flags.device)];
-    #    logvars = [torch.zeros(1, num_samples,
-    #                           self.flags.class_dim).to(self.flags.device)];
-    #    distr_subsets = dict();
-    #    for k, s_key in enumerate(self.subsets.keys()):
-    #        if s_key != '':
-    #            mods = self.subsets[s_key];
-    #            mus_subset = [];
-    #            logvars_subset = [];
-    #            mods_avail = True
-    #            for m, mod in enumerate(mods):
-    #                if mod.name in input_batch.keys():
-    #                    mus_subset.append(enc_mods[mod.name][0].unsqueeze(0));
-    #                    logvars_subset.append(enc_mods[mod.name][1].unsqueeze(0));
-    #                else:
-    #                    mods_avail = False;
-    #            if mods_avail:
-    #                mus_subset = torch.cat(mus_subset, dim=0);
-    #                logvars_subset = torch.cat(logvars_subset, dim=0);
-    #                weights_subset = ((1/float(mus_subset.shape[0]))*
-    #                                  torch.ones(mus_subset.shape[0]).to(self.flags.device));
-    #                s_mu, s_logvar = self.modality_fusion(mus_subset,
-    #                                                      logvars_subset,
-    #                                                      weights_subset);
-    #                distr_subsets[s_key] = [s_mu, s_logvar];
-    #                if self.fusion_condition:
-    #                    mus.append(s_mu.unsqueeze(0));
-    #                    logvars.append(s_logvar.unsqueeze(0));
-    #    mus = torch.cat(mus, dim=0);
-    #    logvars = torch.cat(logvars, dim=0);
-    #    weights = (1/float(mus.shape[0]))*torch.ones(mus.shape[0]).to(self.flags.device);
-    #    joint_mu, joint_logvar = self.moe_fusion(mus, logvars, weights);
-    #    latents['mus'] = mus;
-    #    latents['logvars'] = logvars;
-    #    latents['weights'] = weights;
-    #    latents['joint'] = [joint_mu, joint_logvar];
-    #    latents['subsets'] = distr_subsets;
-    #    return latents;
 
 
     def get_random_styles(self, num_samples):
@@ -255,29 +136,6 @@ class VAEtrimodalSVHNMNIST(BaseMMVae, nn.Module):
         return styles;
 
 
-    #def generate(self, num_samples=None):
-    #    if num_samples is None:
-    #        num_samples = self.flags.batch_size;
-    #    z_class = torch.randn(num_samples, self.flags.class_dim);
-    #    z_class = z_class.to(self.flags.device);
-
-    #    style_latents = self.get_random_styles(num_samples);
-    #    random_latents = {'content': z_class, 'style': style_latents};
-    #    random_samples = self.generate_from_latents(random_latents);
-    #    return random_samples;
-
-
-    #def generate_from_latents(self, latents):
-    #    suff_stats = self.generate_sufficient_statistics_from_latents(latents);
-    #    cond_gen_m1 = suff_stats['mnist'].mean;
-    #    cond_gen_m2 = suff_stats['svhn'].mean;
-    #    cond_gen_m3 = suff_stats['text'].mean;
-    #    cond_gen = {'mnist': cond_gen_m1,
-    #                'svhn': cond_gen_m2,
-    #                'text': cond_gen_m3};
-    #    return cond_gen;
-
-
     def generate_sufficient_statistics_from_latents(self, latents):
         style_m1 = latents['style']['mnist'];
         style_m2 = latents['style']['svhn'];
@@ -287,20 +145,6 @@ class VAEtrimodalSVHNMNIST(BaseMMVae, nn.Module):
         cond_gen_m2 = self.lhood_m2(*self.decoder_m2(style_m2, content));
         cond_gen_m3 = self.lhood_m3(*self.decoder_m3(style_m3, content));
         return {'mnist': cond_gen_m1, 'svhn': cond_gen_m2, 'text': cond_gen_m3}
-
-
-    #def cond_generation(self, latent_distributions, num_samples=None):
-    #    if num_samples is None:
-    #        num_samples = self.flags.batch_size;
-
-    #    style_latents = self.get_random_styles(num_samples);
-    #    cond_gen_samples = dict();
-    #    for k, key in enumerate(latent_distributions.keys()):
-    #        [mu, logvar] = latent_distributions[key];
-    #        content_rep = utils.reparameterize(mu=mu, logvar=logvar);
-    #        latents = {'content': content_rep, 'style': style_latents}
-    #        cond_gen_samples[key] = self.generate_from_latents(latents);
-    #    return cond_gen_samples;
 
 
     def save_networks(self):
