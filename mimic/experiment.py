2b372c65e33a225c0fe1ebc6f35018387f7e03a3
Thomas Sutter <t.sutter@me.com>
2020-09-14 11:37:18 +0200

    Mimic Experiment (first version)

---
+++ mimic/experiment.py
@@ -0,0 +1,170 @@
+
+import os 
+import random
+import numpy as np 
+from itertools import chain, combinations
+
+import torch
+from torchvision import transforms
+import torch.optim as optim
+from sklearn.metrics import average_precision_score
+
+import PIL.Image as Image
+from PIL import ImageFont
+
+from modalities.MimicPA import MimicPA
+from modalities.MimicLateral import MimicLateral
+from modalities.MimicText import MimicText
+
+from mimic.MimicDataset import Mimic
+from mimic.networks.VAEtrimodalMimic import VAEtrimodalMimic
+from mimic.networks.ConvNetworkImgClf import ClfImg as ClfImg
+from mimic.networks.ConvNetworkTextClf import ClfText as ClfText
+
+from mimic.networks.ConvNetworksImgMimic import EncoderImg, DecoderImg
+from mimic.networks.ConvNetworksTextMimic import EncoderText, DecoderText
+
+from utils.BaseExperiment import BaseExperiment
+
+
+class MimicExperiment(BaseExperiment):
+    def __init__(self, flags, alphabet):
+        self.flags = flags;
+        self.name = flags.dataset;
+        self.dataset_name = flags.dataset;
+        self.num_modalities = flags.num_mods;
+        self.plot_img_size = torch.Size((1, 128, 128))
+        self.font = ImageFont.truetype('FreeSerif.ttf', 38)
+
+        self.alphabet = alphabet;
+        self.flags.num_features = len(alphabet);
+
+        self.modalities = self.set_modalities();
+        self.subsets = self.set_subsets();
+        self.dataset_train = None;
+        self.dataset_test = None;
+        self.set_dataset();
+
+        self.mm_vae = self.set_model();
+        self.clfs = self.set_clfs();
+        self.optimizer = None;
+        self.rec_weights = self.set_rec_weights();
+        self.style_weights = self.set_style_weights();
+
+        self.test_samples = self.get_test_samples();
+        self.eval_metric = average_precision_score; 
+        self.paths_fid = self.set_paths_fid();
+
+        self.labels = ['Lung Opacity', 'Pleural Effusion', 'Support Devices'];
+
+    def set_model(self):
+        model = VAEtrimodalMimic(self.flags, self.modalities, self.subsets)
+        model = model.to(self.flags.device);
+        return model;
+
+
+    def set_modalities(self):
+        mod1 = MimicPA(EncoderImg(self.flags, self.flags.style_pa_dim),
+                       DecoderImg(self.flags, self.flags.style_pa_dim));
+        mod2 = MimicLateral(EncoderImg(self.flags, self.flags.style_lat_dim),
+                            DecoderImg(self.flags, self.flags.style_lat_dim));
+        mod3 = MimicText(EncoderText(self.flags, self.flags.style_text_dim),
+                         DecoderText(self.flags, self.flags.style_text_dim),
+                         self.flags.len_sequence,
+                         self.alphabet,
+                         self.plot_img_size,
+                         self.font);
+        mods = {mod1.name: mod1, mod2.name: mod2, mod3.name: mod3};
+        return mods;
+
+
+    def set_dataset(self):
+        d_train = Mimic(self.flags, self.alphabet, dataset=1)
+        d_eval = Mimic(self.flags, self.alphabet, dataset=2)
+        self.dataset_train = d_train;
+        self.dataset_test = d_eval;
+
+
+    def set_clfs(self):
+        model_clf_m1 = None;
+        model_clf_m2 = None;
+        model_clf_m3 = None;
+        if self.flags.use_clf:
+            model_clf_m1 = ClfImg(self.flags);
+            model_clf_m1.load_state_dict(torch.load(os.path.join(self.flags.dir_clf,
+                                                                 self.flags.clf_save_m1)))
+            model_clf_m1 = model_clf_m1.to(self.flags.device);
+
+            model_clf_m2 = ClfImg(self.flags);
+            model_clf_m2.load_state_dict(torch.load(os.path.join(self.flags.dir_clf,
+                                                                 self.flags.clf_save_m2)))
+            model_clf_m2 = model_clf_m2.to(self.flags.device);
+
+            model_clf_m3 = ClfText(self.flags);
+            model_clf_m3.load_state_dict(torch.load(os.path.join(self.flags.dir_clf,
+                                                                 self.flags.clf_save_m3)))
+            model_clf_m3 = model_clf_m3.to(self.flags.device);
+
+        clfs = {'PA': model_clf_m1,
+                'Lateral': model_clf_m2,
+                'text': model_clf_m3}
+        return clfs;
+
+
+    def set_optimizer(self):
+        # optimizer definition
+        optimizer = optim.Adam(
+            list(self.mm_vae.parameters()),
+            lr=self.flags.initial_learning_rate,
+            betas=(self.flags.beta_1, self.flags.beta_2))
+        self.optimizer = optimizer;
+
+
+    def set_rec_weights(self):
+        rec_weights = dict();
+        ref_mod_d_size = self.modalities['PA'].data_size.numel();
+        for k, m_key in enumerate(self.modalities.keys()):
+            mod = self.modalities[m_key];
+            numel_mod = mod.data_size.numel()
+            rec_weights[mod.name] = float(ref_mod_d_size/numel_mod)
+        return rec_weights;
+
+
+    def set_style_weights(self):
+        weights = dict();
+        weights['PA'] = self.flags.beta_m1_style;
+        weights['Lateral'] = self.flags.beta_m2_style;
+        weights['text'] = self.flags.beta_m3_style;
+        return weights;
+
+
+    def get_test_samples(self, num_images=10):
+        n_test = self.dataset_test.__len__();
+        samples = []
+        for i in range(num_images):
+            while True:
+                sample, target = self.dataset_test.__getitem__(random.randint(0, n_test))
+                if target == i:
+                    for k, key in enumerate(sample):
+                        sample[key] = sample[key].to(self.flags.device);
+                    samples.append(sample)
+                    break;
+        return samples
+
+
+    def mean_eval_metric(self, values):
+        return np.mean(np.array(values));
+
+
+    def set_paths_fid(self):
+        dir_real = os.path.join(self.flags.dir_gen_eval_fid, 'real');
+        dir_random = os.path.join(self.flags.dir_gen_eval_fid, 'random');
+        paths = {'real': dir_real,
+                 'random': dir_random}
+        dir_cond = self.flags.dir_gen_eval_fid;
+        for k, name in enumerate(self.subsets):
+            paths[name] = os.path.join(dir_cond, name);
+        return paths;
+
+
+
--- mimic/networks/VAEtrimodalMimic.py
+++ mimic/networks/VAEtrimodalMimic.py
@@ -14,272 +14,94 @@ from divergence_measures.mm_div import calc_kl_divergence
 from divergence_measures.mm_div import poe
 
 from utils import utils
+from utils.BaseMMVae import BaseMMVae
 
 
-class VAEtrimodalMimic(nn.Module):
-    def __init__(self, flags):
+class VAEtrimodalMimic(BaseMMVae, nn.Module):
+    def __init__(self, flags, modalities, subsets):
         super(VAEtrimodalMimic, self).__init__()
-        self.num_modalities = 3;
-        self.flags = flags;
-        self.encoder_pa = EncoderImg(flags, flags.style_pa_dim)
-        self.encoder_lat = EncoderImg(flags, flags.style_lat_dim)
-        self.encoder_text = EncoderText(flags, flags.style_text_dim)
-        self.decoder_pa = DecoderImg(flags, flags.style_pa_dim);
-        self.decoder_lat = DecoderImg(flags, flags.style_lat_dim);
-        self.decoder_text = DecoderText(flags, flags.style_text_dim);
+        self.encoder_pa = modalities['PA'].encoder;
+        self.encoder_lat = modalities['Lateral'].encoder;
+        self.encoder_text = modalities['text'].encoder;
+        self.decoder_pa = modalities['PA'].text;
+        self.decoder_lat = modalities['Lateral'].decoder;
+        self.decoder_text = modalities['text'].decoder;
         self.encoder_pa = self.encoder_pa.to(flags.device);
         self.encoder_lat = self.encoder_lat.to(flags.device);
         self.encoder_text = self.encoder_text.to(flags.device);
         self.decoder_pa = self.decoder_pa.to(flags.device);
         self.decoder_lat = self.decoder_lat.to(flags.device);
         self.decoder_text = self.decoder_text.to(flags.device);
-        self.lhood_pa = utils.get_likelihood(flags.likelihood_m1);
-        self.lhood_lat = utils.get_likelihood(flags.likelihood_m2);
-        self.lhood_text = utils.get_likelihood(flags.likelihood_m3);
-
-        d_size_m1 = flags.img_size*flags.img_size;
-        d_size_m2 = flags.img_size*flags.img_size;
-        d_size_m3 = flags.len_sequence;
-        total_d_size = d_size_m1 + d_size_m2 + d_size_m3;
-        w1 = 1.0;
-        w2 = d_size_m1/d_size_m2;
-        w3 = d_size_m1/d_size_m3;
-        w_total = w1+w2+w3;
-        self.rec_w1 = w1;
-        self.rec_w2 = w2;
-        self.rec_w3 = w3;
-        weights = utils.reweight_weights(torch.Tensor(flags.alpha_modalities));
-        self.weights = weights.to(flags.device);
-        if flags.modality_moe or flags.modality_jsd:
-            self.modality_fusion = self.moe_fusion;
-            if flags.modality_moe:
-                self.calc_joint_divergence = self.divergence_moe;
-            if flags.modality_jsd:
-                self.calc_joint_divergence = self.divergence_jsd;
-        elif flags.modality_poe:
-            self.modality_fusion = self.poe_fusion;
-            self.calc_joint_divergence = self.divergence_poe;
+        self.lhood_pa = modalities['PA'].likelihood;
+        self.lhood_lat = modalities['Lateral'].likelihood;
+        self.lhood_text = modalities['text'].likelihood;
 
 
-    def forward(self, input_pa=None, input_lat=None, input_text=None):
-        latents = self.inference(input_pa, input_lat, input_text);
+    def forward(self, input_batch):
+        latents = self.inference(input_batch);
         results = dict();
         results['latents'] = latents;
         div = self.calc_joint_divergence(latents['mus'],
                                          latents['logvars'],
                                          latents['weights']);
-        for k, key in enumerate(div.keys()):
-            results[key] = div[key];
-
         results['group_distr'] = latents['joint'];
         class_embeddings = utils.reparameterize(latents['joint'][0],
                                                 latents['joint'][1]);
 
+        for k, key in enumerate(div.keys()):
+            results[key] = div[key];
+
+        input_m1 = input_batch['PA'];
+        input_m2 = input_batch['Lateral'];
+        input_m3 = input_batch['text'];
         results_rec = dict();
-        if input_pa is not None:
-            m1_s_mu, m1_s_logvar = latents['pa'][:2];
-            if self.flags.factorized_representation:
-                m1_s_embeddings = utils.reparameterize(mu=m1_s_mu, logvar=m1_s_logvar);
-            else:
-                m1_s_embeddings = None;
-            m1_rec = self.lhood_pa(*self.decoder_pa(m1_s_embeddings, class_embeddings));
-            results_rec['pa'] = m1_rec;
-        if input_lat is not None:
-            m2_s_mu, m2_s_logvar = latents['lateral'][:2];
-            if self.flags.factorized_representation:
-                m2_s_embeddings = utils.reparameterize(mu=m2_s_mu, logvar=m2_s_logvar);
-            else:
-                m2_s_embeddings = None;
-            m2_rec = self.lhood_lat(*self.decoder_lat(m2_s_embeddings, class_embeddings));
-            results_rec['lateral'] = m2_rec;
-        if input_text is not None:
-            m3_s_mu, m3_s_logvar = latents['text'][:2];
-            if self.flags.factorized_representation:
-                m3_s_embeddings = utils.reparameterize(mu=m3_s_mu, logvar=m3_s_logvar);
-            else:
-                m3_s_embeddings = None;
-            m3_rec = self.lhood_text(*self.decoder_text(m3_s_embeddings, class_embeddings));
-            results_rec['text'] = m3_rec;
+        for k, m_key in enumerate(self.modalities.keys()):
+            mod = self.modalities[m_key]
+            input_mod = input_batch[m_key];
+            if input_mod is not None:
+                s_mu, s_logvar = latents[m_key + '_style'];
+                if self.flags.factorized_representation:
+                    s_emb = utils.reparameterize(mu=s_mu, logvar=s_logvar);
+                else:
+                    s_emb = None;
+                if m_key == 'PA':
+                    rec = self.lhood_pa(*self.decoder_pa(s_emb, class_embeddings));
+                elif m_key == 'Lateral':
+                    rec = self.lhood_lat(*self.decoder_lat(s_emb, class_embeddings));
+                    lhood = self.lhood_lat;
+                elif m_key == 'text':
+                    rec = self.lhood_text(*self.decoder_text(s_emb, class_embeddings));
+                results_rec[m_key] = rec;
         results['rec'] = results_rec;
         return results;
 
 
-    def divergence_poe(self, mus, logvars, weights=None):
-        div_measures = calc_group_divergence_poe(self.flags,
-                                         mus,
-                                         logvars,
-                                         norm=self.flags.batch_size);
-        divs = dict();
-        divs['joint_divergence'] = div_measures[0];
-        divs['individual_divs'] = div_measures[1];
-        divs['dyn_prior'] = None;
-        return divs;
-
-
-    def divergence_moe(self, mus, logvars, weights=None):
-        if weights is None:
-            weights=self.weights;
-        weights = weights.clone();
-        weights[0] = 0.0;
-        weights = utils.reweight_weights(weights);
-        div_measures = calc_group_divergence_moe(self.flags,
-                                                 mus,
-                                                 logvars,
-                                                 weights,
-                                                 normalization=self.flags.batch_size);
-        divs = dict();
-        divs['joint_divergence'] = div_measures[0];
-        divs['individual_divs'] = div_measures[1];
-        divs['dyn_prior'] = None;
-        return divs;
-
-
-    def divergence_moe_poe(self, mus, logvars, weights=None):
-        if weights is None:
-            weights=self.weights;
-        weights = weights.clone();
-        weights[0] = 0.0;
-        weights = utils.reweight_weights(weights);
-        div_measures = calc_group_divergence_moe(self.flags,
-                                                 mus,
-                                                 logvars,
-                                                 weights,
-                                                 normalization=self.flags.batch_size);
-        divs = dict();
-        divs['joint_divergence'] = div_measures[0];
-        divs['individual_divs'] = div_measures[1];
-        divs['dyn_prior'] = None;
-        return divs;
-
-
-    def divergence_jsd(self, mus, logvars, weights=None):
-        if weights is None:
-            weights=self.weights;
-        weights = weights.clone();
-        weights = utils.reweight_weights(weights);
-        div_measures = calc_alphaJSD_modalities(self.flags,
-                                                mus,
-                                                logvars,
-                                                weights,
-                                                normalization=self.flags.batch_size);
-        divs = dict();
-        divs['joint_divergence'] = div_measures[0];
-        divs['individual_divs'] = div_measures[1];
-        divs['dyn_prior'] = div_measures[2];
-        return divs;
-
-
-
-    def moe_fusion(self, mus, logvars, weights=None):
-        if weights is None:
-            weights = self.weights;
-        mu_moe, logvar_moe = utils.mixture_component_selection(self.flags,
-                                                               mus,
-                                                               logvars,
-                                                               weights);
-        return [mu_moe, logvar_moe];
-
-
-    def poe_fusion(self, mus, logvars, weights=None):
-        mu_poe, logvar_poe = poe(mus, logvars);
-        return [mu_poe, logvar_poe];
-
-
-    def encode(self, i_pa=None, i_lat=None, i_text=None):
+    def encode(self, input_batch):
         latents = dict();
-        if i_pa is not None:
-            latents['pa'] = self.encoder_pa(i_pa)
-            latents['pa_style'] = latents['pa'][:2]
-            latents['pa'] = latents['pa'][2:]
+        if 'PA' in input_batch.keys():
+            i_m1 = input_batch['PA'];
+            latents['PA'] = self.encoder_pa(i_m1)
+            latents['PA_style'] = latents['PA'][:2]
+            latents['PA'] = latents['PA'][2:]
         else:
-            latents['pa_style'] = [None, None]
-            latents['pa'] = [None, None]
-        if i_lat is not None:
-            latents['lateral'] = self.encoder_lat(i_lat)
-            latents['lateral_style'] = latents['lateral'][:2]
-            latents['lateral'] = latents['lateral'][2:]
+            latents['PA_style'] = [None, None]
+            latents['PA'] = [None, None]
+        if 'PA' in input_batch.keys():
+            i_m2 = input_batch['Lateral'];
+            latents['Lateral'] = self.encoder_lat(i_m2)
+            latents['Lateral_style'] = latents['Lateral'][:2]
+            latents['Lateral'] = latents['Lateral'][2:]
         else:
-            latents['lateral_style'] = [None, None]
-            latents['lateral'] = [None, None]
-        if i_text is not None:
-            latents['text'] = self.encoder_text(i_lat)
+            latents['Lateral_style'] = [None, None]
+            latents['Lateral'] = [None, None]
+        if 'text' in input_batch.keys():
+            i_m3 = input_batch['text'];
+            latents['text'] = self.encoder_text(i_m3)
             latents['text_style'] = latents['text'][:2]
             latents['text'] = latents['text'][2:]
         else:
             latents['text_style'] = [None, None]
             latents['text'] = [None, None]
-        return latents;
-
-
-    def inference(self, input_pa=None, input_lat=None, input_text=None):
-        latents = self.encode(i_pa=input_pa,
-                              i_lat=input_lat,
-                              i_text=input_text);
-        if input_pa is not None:
-            num_samples = input_pa.shape[0];
-        if input_lat is not None:
-            num_samples = input_lat.shape[0];
-        if input_text is not None:
-            num_samples = input_text.shape[0];
-        weights = [self.weights[0]];
-        mus = torch.zeros(1, num_samples, self.flags.class_dim).to(self.flags.device);
-        logvars = torch.zeros(1, num_samples, self.flags.class_dim).to(self.flags.device);
-        if input_pa is not None:
-            pa_mu, pa_logvar = latents['pa'][2:];
-            mus = torch.cat([mus, pa_mu.unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, pa_logvar.unsqueeze(0)], dim=0);
-            weights.append(self.weights[1]);
-        if input_lat is not None:
-            lat_mu, lat_logvar = latents['lateral'][2:];
-            mus = torch.cat([mus, lat_mu.unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, lat_logvar.unsqueeze(0)], dim=0);
-            weights.append(self.weights[2]);
-        if input_text is not None:
-            text_mu, text_logvar = latents['text'][2:];
-            mus = torch.cat([mus, text_mu.unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, text_logvar.unsqueeze(0)], dim=0);
-            weights.append(self.weights[3]);
-        if input_pa is not None and input_lat is not None:
-            poe_pa_lat = poe(torch.cat([pa_mu.unsqueeze(0),
-                                        lat_mu.unsqueeze(0)], dim=0),
-                             torch.cat([pa_logvar.unsqueeze(0),
-                                        lat_logvar.unsqueeze(0)], dim=0))
-            latents['pa_lat'] = poe_pa_lat;
-            mus = torch.cat([mus, poe_pa_lat[0].unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, poe_pa_lat[1].unsqueeze(0)], dim=0);
-        if input_pa is not None and input_text is not None:
-            poe_pa_text = poe(torch.cat([pa_mu.unsqueeze(0),
-                                         text_mu.unsqueeze(0)], dim=0),
-                              torch.cat([pa_logvar.unsqueeze(0),
-                                         text_logvar.unsqueeze(0)], dim=0))
-            latents['pa_text'] = poe_pa_text;
-            mus = torch.cat([mus, poe_pa_text[0].unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, poe_pa_text[1].unsqueeze(0)], dim=0);
-        if input_lat is not None and input_text is not None:
-            poe_lat_text = poe(torch.cat([lat_mu.unsqueeze(0),
-                                          text_mu.unsqueeze(0)], dim=0),
-                               torch.cat([lat_logvar.unsqueeze(0),
-                                          text_logvar.unsqueeze(0)], dim=0))
-            latents['lat_text'] = poe_lat_text;
-            mus = torch.cat([mus, poe_lat_text[0].unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, poe_lat_text[1].unsqueeze(0)], dim=0);
-        if input_pa is not None and input_lat is not None and input_text is not None:
-            poe_pa_lat_text = poe(torch.cat([pa_mu.unsqueeze(0),
-                                             lat_mu.unsqueeze(0),
-                                             text_mu.unsqueeze(0)], dim=0),
-                                  torch.cat([pa_logvar.unsqueeze(0),
-                                             lat_logvar.unsqueeze(0),
-                                             text_logvar.unsqueeze(0)], dim=0))
-            latents['pa_lat_text'] = poe_pa_lat_text;
-            mus = torch.cat([mus, poe_pa_lat_text[0].unsqueeze(0)], dim=0);
-            logvars = torch.cat([logvars, poe_pa_lat_text[1].unsqueeze(0)], dim=0);
-
-        weights = (1/float(mus.shape[0]))*torch.ones(mus.shape[0]).to(self.flags.device);
-        joint_mu, joint_logvar = self.modality_fusion(mus, logvars, weights);
-        latents['mus'] = mus;
-        latents['logvars'] = logvars;
-        latents['weights'] = weights;
-        latents['joint'] = [joint_mu, joint_logvar];
         return latents;
 
 
@@ -295,7 +117,7 @@ class VAEtrimodalMimic(nn.Module):
             z_style_1 = None;
             z_style_2 = None;
             z_style_3 = None;
-        styles = {'pa': z_style_1, 'lateral': z_style_2, 'text': z_style_3};
+        styles = {'PA': z_style_1, 'Lateral': z_style_2, 'text': z_style_3};
         return styles;
 
 
@@ -315,7 +137,7 @@ class VAEtrimodalMimic(nn.Module):
         m1_dist = [s1_mu, s1_logvar];
         m2_dist = [s2_mu, s2_logvar];
         m3_dist = [s3_mu, s3_logvar];
-        styles = {'pa': m1_dist, 'lateral': m2_dist, 'text': m3_dist};
+        styles = {'PA': m1_dist, 'Lateral': m2_dist, 'text': m3_dist};
         return styles;
 
 
@@ -333,64 +155,24 @@ class VAEtrimodalMimic(nn.Module):
 
     def generate_from_latents(self, latents):
         suff_stats = self.generate_sufficient_statistics_from_latents(latents);
-        cond_gen_pa = suff_stats['pa'].mean;
-        cond_gen_lat = suff_stats['lateral'].mean;
+        cond_gen_pa = suff_stats['PA'].mean;
+        cond_gen_lat = suff_stats['Lateral'].mean;
         cond_gen_text = suff_stats['text'].mean;
-        cond_gen = {'pa': cond_gen_pa,
-                    'lateral': cond_gen_lat,
+        cond_gen = {'PA': cond_gen_pa,
+                    'Lateral': cond_gen_lat,
                     'text': cond_gen_text};
         return cond_gen;
 
 
     def generate_sufficient_statistics_from_latents(self, latents):
-        style_pa = latents['style']['pa'];
-        style_lat = latents['style']['lateral'];
+        style_pa = latents['style']['PA'];
+        style_lat = latents['style']['Lateral'];
         style_text = latents['style']['text'];
         content = latents['content']
         cond_gen_m1 = self.lhood_pa(*self.decoder_pa(style_pa, content));
         cond_gen_m2 = self.lhood_lat(*self.decoder_lat(style_lat, content));
         cond_gen_m3 = self.lhood_text(*self.decoder_text(style_text, content));
-        return {'pa': cond_gen_m1, 'lateral': cond_gen_m2, 'text': cond_gen_m3}
-
-
-    def cond_generation_1a(self, latent_distributions, num_samples=None):
-        if num_samples is None:
-            num_samples = self.flags.batch_size;
-
-        style_latents = self.get_random_styles(num_samples);
-        cond_gen_samples = dict();
-        for k, key in enumerate(latent_distributions):
-            [mu, logvar] = latent_distributions[key];
-            content_rep = utils.reparameterize(mu=mu, logvar=logvar);
-            latents = {'content': content_rep, 'style': style_latents}
-            cond_gen_samples[key] = self.generate_from_latents(latents);
-        return cond_gen_samples;
-
-
-    def cond_generation_2a(self, latent_distribution_pairs, num_samples=None,
-                           dp_gen=False):
-        if num_samples is None:
-            num_samples = self.flags.batch_size;
-
-        style_latents = self.get_random_styles(num_samples);
-        cond_gen_2a = dict();
-        for p, pair in enumerate(latent_distribution_pairs.keys()):
-            ld_pair = latent_distribution_pairs[pair];
-            mu_list = []; logvar_list = [];
-            for k, key in enumerate(ld_pair['latents'].keys()):
-                mu_list.append(ld_pair['latents'][key][0].unsqueeze(0));
-                logvar_list.append(ld_pair['latents'][key][1].unsqueeze(0));
-            mus = torch.cat(mu_list, dim=0);
-            logvars = torch.cat(logvar_list, dim=0);
-            weights_pair = utils.reweight_weights(torch.Tensor(ld_pair['weights']));
-            if dp_gen:
-                mu_joint, logvar_joint = poe(mus, logvars);
-            else:
-                mu_joint, logvar_joint = self.modality_fusion(mus, logvars, weights_pair)
-            c_emb = utils.reparameterize(mu_joint, logvar_joint);
-            l_2a = {'content': c_emb, 'style': style_latents};
-            cond_gen_2a[pair] = self.generate_from_latents(l_2a);
-        return cond_gen_2a;
+        return {'PA': cond_gen_m1, 'Lateral': cond_gen_m2, 'text': cond_gen_m3}
 
 
     def save_networks(self):
