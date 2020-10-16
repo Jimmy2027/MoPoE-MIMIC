d4d0dc66b498e61de5cf065544c2c54496602c2d
klugh <klugh@student.ethz.ch>
2020-10-16 12:35:04 +0200

    added test for model training workflow

---
+++ mimic/tests/tests_training.py
@@ -0,0 +1,61 @@
+import argparse
+import json
+import os
+import tempfile
+from unittest import TestCase
+
+import torch
+
+from mimic.experiment import MimicExperiment
+from mimic.flags import parser
+from run_epochs import run_epochs
+from utils.filehandling import create_dir_structure, expand_paths, create_dir_structure_testing, get_config_path
+
+
+class TestTraining(TestCase):
+    def test_train_loop(self):
+        """
+        General test to see if training loop works
+        """
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            FLAGS = parser.parse_args([])
+            config_path = get_config_path()
+            with open(config_path, 'rt') as json_file:
+                t_args = argparse.Namespace()
+                t_args.__dict__.update(json.load(json_file))
+                FLAGS = parser.parse_args([], namespace=t_args)
+            FLAGS = expand_paths(t_args)
+            use_cuda = torch.cuda.is_available()
+            FLAGS.device = torch.device('cuda' if use_cuda else 'cpu')
+            if FLAGS.method == 'poe':
+                FLAGS.modality_poe = True
+                FLAGS.poe_unimodal_elbos = True
+            elif FLAGS.method == 'moe':
+                FLAGS.modality_moe = True
+            elif FLAGS.method == 'jsd':
+                FLAGS.modality_jsd = True
+            elif FLAGS.method == 'joint_elbo':
+                FLAGS.joint_elbo = True
+            else:
+                NotImplementedError('method not implemented...exit!')
+
+            FLAGS.alpha_modalities = [FLAGS.div_weight_uniform_content, FLAGS.div_weight_m1_content,
+                                      FLAGS.div_weight_m2_content, FLAGS.div_weight_m3_content]
+
+            FLAGS.dir_experiment = tmpdirname
+            FLAGS.dataset = 'testing'
+            FLAGS.use_clf = False
+            FLAGS.end_epoch = 2
+            FLAGS.batch_size = 10
+            FLAGS.eval_freq = 1
+
+            FLAGS = create_dir_structure(FLAGS)
+            alphabet_path = os.path.join(os.getcwd(), 'alphabet.json')
+            with open(alphabet_path) as alphabet_file:
+                alphabet = str(''.join(json.load(alphabet_file)))
+
+            mimic = MimicExperiment(FLAGS, alphabet)
+            create_dir_structure_testing(mimic)
+            mimic.set_optimizer()
+
+            run_epochs(mimic)
