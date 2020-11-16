65f54190432ce6b40709142cf92fdf102ae42969
klugh <klugh@student.ethz.ch>
2020-11-16 21:28:04 +0100

    added "get_..."

--- mimic/dataio/utils.py
+++ mimic/dataio/utils.py
@@ -1,23 +1,32 @@
+from torch.utils.data import DataLoader
 from torchvision import transforms
 import torch
 import PIL.Image as Image
 
 
-def get_transform_img(args):
+def get_transform_img(args: any):
     if args.img_clf_type == 'cheXnet' or args.feature_extractor_img == 'densenet':
         normalize = transforms.Normalize([0.485, 0.456, 0.406],
                                          [0.229, 0.224, 0.225])
         crops_transform = get_crops_transform(args)
-        transform_img = transforms.Compose([
+        transformation_list = [
             transforms.ToPILImage(),
             transforms.Lambda(lambda x: x.convert('RGB')),
-            transforms.Resize(args.img_size),
-            crops_transform,
-            transforms.Lambda
-            (lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
-            transforms.Lambda
-            (lambda crops: torch.stack([normalize(crop) for crop in crops]))
-        ])
+            transforms.Resize(args.img_size)]
+        if args.n_crops not in [10, 5]:
+            transformation_list.extend([transforms.ToTensor(), normalize])
+        else:
+            if args.n_crops == 10:
+                transformation_list.append(transforms.TenCrop(224))
+            elif args.n_crops == 5:
+                crops_transform = transforms.FiveCrop(224)
+
+            transformation_list.extend([crops_transform, transforms.Lambda(
+                lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+                                        transforms.Lambda(
+                                            lambda crops: torch.stack([normalize(crop) for crop in crops]))])
+        transform_img = transforms.Compose(transformation_list)
+
     else:
         transform_img = transforms.Compose([
             transforms.ToPILImage(),
@@ -29,7 +38,7 @@ def get_transform_img(args):
     return transform_img
 
 
-def get_crops_transform(args):
+def get_crops_transform(args) -> transforms:
     if args.n_crops == 10:
         crops_transform = transforms.TenCrop(224)
     elif args.n_crops == 5:
@@ -38,3 +47,24 @@ def get_crops_transform(args):
         # ugly...
         crops_transform = transforms.Lambda(lambda x: x)
     return crops_transform
+
+
+def get_data_loaders(args, dataset):
+    if args.distributed:
+        sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        num_workers = args.dataloader_workers // args.world_size
+    else:
+        sampler = None
+        num_workers = args.dataloader_workers
+    d_loader = DataLoader(dataset, batch_size=args.batch_size,
+                          shuffle=(sampler is None),
+                          num_workers=num_workers, drop_last=True,
+                          sampler=sampler, pin_memory=True)
+
+    return sampler, d_loader
+
+
+def samplers_set_epoch(args, train_sampler, test_sampler, epoch: int) -> None:
+    if args.distributed:
+        for sampler in [train_sampler, test_sampler]:
+            sampler.set_epoch(epoch)
