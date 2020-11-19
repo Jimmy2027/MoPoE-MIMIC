a9fb74918d91ba1914b64f454b5debf6535955d6
klugh <klugh@student.ethz.ch>
2020-11-19 15:40:32 +0100

    added get random labels

--- mimic/evaluation/eval_metrics/representation.py
+++ mimic/evaluation/eval_metrics/representation.py
@@ -16,25 +16,30 @@ def train_clf_lr_all_subsets(exp: MimicExperiment):
 
     d_loader = DataLoader(exp.dataset_train, batch_size=exp.flags.batch_size,
                           shuffle=True,
-                          num_workers=args.dataloader_workers // args.world_size if args.distributed else args.dataloader_workers,
+                          num_workers=args.dataloader_workers // args.world_size if args.distributed
+                          else args.dataloader_workers,
                           drop_last=True)
-    if exp.flags.dataset == 'testing':
+    if exp.flags.steps_per_training_epoch > 0:
         training_steps = exp.flags.steps_per_training_epoch
     else:
         training_steps = len(d_loader)
 
     bs = exp.flags.batch_size
-    num_batches_epoch = int(exp.dataset_train.__len__() / float(bs))
     class_dim = exp.flags.class_dim
     n_samples = int(exp.dataset_train.__len__())
-    data_train = dict()
-    for k, s_key in enumerate(subsets.keys()):
-        if s_key != '':
-            data_train[s_key] = np.zeros((n_samples,
-                                          class_dim))
+    data_train = {
+        s_key: np.zeros((n_samples, class_dim))
+        for k, s_key in enumerate(subsets.keys())
+        if s_key != ''
+    }
+
     all_labels = np.zeros((n_samples, len(exp.labels)))
     for it, (batch_d, batch_l) in tqdm(enumerate(d_loader), total=training_steps, postfix='train_clf_lr'):
-        if it > training_steps:
+        """
+        Constructs the training set (labels and inferred subsets) for the classifier training.
+        """
+        if it > training_steps and len(np.unique(all_labels)) > 1:
+            # labels need at least 2 classes to train the clf
             break
 
         batch_d = {k: v.to(exp.flags.device) for k, v in batch_d.items()}
@@ -47,14 +52,32 @@ def train_clf_lr_all_subsets(exp: MimicExperiment):
             data_train[key][(it * bs):((it + 1) * bs), :] = lr_subsets[key][0].cpu().data.numpy()
 
     n_train_samples = exp.flags.num_training_samples_lr
-    rand_ind_train = np.random.randint(n_samples, size=n_train_samples)
-    labels = all_labels[rand_ind_train, :]
+    # get random labels such that it contains both classes
+    labels, rand_ind_train = get_random_labels(n_samples, n_train_samples, all_labels)
     for k, s_key in enumerate(subsets.keys()):
         if s_key != '':
             d = data_train[s_key]
             data_train[s_key] = d[rand_ind_train, :]
-    clf_lr = train_clf_lr(exp, data_train, labels)
-    return clf_lr
+    return train_clf_lr(exp, data_train, labels)
+
+
+def get_random_labels(n_samples, n_train_samples, all_labels, max_tries=1000):
+    """
+    The classifier needs labels from both classes to train. This function resamples "all_labels"
+    until it contains examples from both classes
+    """
+    assert any(len(np.unique(all_labels[:, l])) > 1 for l in range(all_labels.shape[-1])), \
+        'The labels must contain at least two classes to train the classifier'
+    rand_ind_train = np.random.randint(n_samples, size=n_train_samples)
+    labels = all_labels[rand_ind_train, :]
+    tries = 1
+    while any(len(np.unique(labels[:, l])) <= 1 for l in range(labels.shape[-1])):
+        rand_ind_train = np.random.randint(n_samples, size=n_train_samples)
+        labels = all_labels[rand_ind_train, :]
+        tries += 1
+        assert max_tries >= tries, f'Could not get sample containing both classes to train ' \
+                                   f'the classifier in {tries} tries. Might need to increase batch_size'
+    return labels, rand_ind_train
 
 
 def test_clf_lr_all_subsets(epoch, clf_lr, exp):
@@ -63,27 +86,36 @@ def test_clf_lr_all_subsets(epoch, clf_lr, exp):
     mm_vae.eval()
     subsets = exp.subsets
 
-    lr_eval = dict()
-    for l, label_str in enumerate(exp.labels):
-        lr_eval[label_str] = dict()
-        for k, s_key in enumerate(subsets.keys()):
-            if s_key != '':
-                lr_eval[label_str][s_key] = []
+    lr_eval = {
+        label_str: {
+            s_key: [] for k, s_key in enumerate(subsets.keys()) if s_key != ''
+        }
+        for l, label_str in enumerate(exp.labels)
+    }
 
     d_loader = DataLoader(exp.dataset_test, batch_size=exp.flags.batch_size,
                           shuffle=True,
                           num_workers=exp.flags.dataloader_workers, drop_last=True)
 
+    if exp.flags.steps_per_training_epoch > 0:
+        training_steps = exp.flags.steps_per_training_epoch
+    else:
+        training_steps = len(d_loader)
+
     for iteration, batch in enumerate(d_loader):
+        if iteration > training_steps:
+            break
         batch_d = batch[0]
         batch_l = batch[1]
         batch_d = {k: v.to(exp.flags.device) for k, v in batch_d.items()}
 
         inferred = mm_vae.module.inference(batch_d) if args.distributed else mm_vae.inference(batch_d)
         lr_subsets = inferred['subsets']
-        data_test = dict()
-        for k, key in enumerate(lr_subsets.keys()):
-            data_test[key] = lr_subsets[key][0].cpu().data.numpy()
+        data_test = {
+            key: lr_subsets[key][0].cpu().data.numpy()
+            for k, key in enumerate(lr_subsets.keys())
+        }
+
         evals = classify_latent_representations(exp,
                                                 epoch,
                                                 clf_lr,
@@ -102,11 +134,11 @@ def test_clf_lr_all_subsets(epoch, clf_lr, exp):
 
 def classify_latent_representations(exp, epoch, clf_lr, data, labels):
     labels = np.array(np.reshape(labels, (labels.shape[0], len(exp.labels))))
-    eval_all_labels = dict()
+    eval_all_labels = {}
     for l, label_str in enumerate(exp.labels):
         gt = labels[:, l]
         clf_lr_label = clf_lr[label_str]
-        eval_all_reps = dict()
+        eval_all_reps = {}
         for s_key in data.keys():
             data_rep = data[s_key]
             clf_lr_rep = clf_lr_label[s_key]
@@ -124,10 +156,10 @@ def classify_latent_representations(exp, epoch, clf_lr, data, labels):
 
 def train_clf_lr(exp, data, labels):
     labels = np.reshape(labels, (labels.shape[0], len(exp.labels)))
-    clf_lr_labels = dict()
+    clf_lr_labels = {}
     for l, label_str in enumerate(exp.labels):
         gt = labels[:, l]
-        clf_lr_reps = dict()
+        clf_lr_reps = {}
         for s_key in data.keys():
             data_rep = data[s_key]
             clf_lr_s = LogisticRegression(random_state=0, solver='lbfgs', multi_class='auto', max_iter=1000)
