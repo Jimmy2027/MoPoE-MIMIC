a1e279815e9298a609e42f036117ba937d5919db
klugh <klugh@student.ethz.ch>
2020-11-21 09:24:27 +0100

    added max training steps for testing

--- mimic/evaluation/eval_metrics/representation.py
+++ mimic/evaluation/eval_metrics/representation.py
@@ -38,9 +38,11 @@ def train_clf_lr_all_subsets(exp: MimicExperiment):
         """
         Constructs the training set (labels and inferred subsets) for the classifier training.
         """
-        # if it > training_steps and len(np.unique(all_labels)) > 1:
-        #     # labels need at least 2 classes to train the clf
-        #     break
+        if it > training_steps \
+                and any(len(np.unique(all_labels[:, l])) > 1 for l in range(all_labels.shape[-1])) \
+                and it > 150:
+            # labels need at least 2 classes to train the clf
+            break
 
         batch_d = {k: v.to(exp.flags.device) for k, v in batch_d.items()}
         inferred = mm_vae.module.inference(batch_d) if args.distributed else mm_vae.inference(batch_d)
