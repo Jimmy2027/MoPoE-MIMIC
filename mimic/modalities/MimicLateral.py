a573626941fbfacd88e6be9de6c32b1be82a02f4
klugh <klugh@student.ethz.ch>
2020-12-12 11:27:09 +0100

    added class ModalityIMG

--- mimic/modalities/MimicLateral.py
+++ mimic/modalities/MimicLateral.py
@@ -1,34 +1,17 @@
-import PIL.Image as Image
 import torch
-from torchvision import transforms
 
 import mimic.modalities.utils
-from mimic.modalities.Modality import Modality
-from mimic.utils import utils
-from mimic.utils.save_samples import write_samples_img_to_file
+from mimic.modalities.Modality import ModalityIMG
 
 
-class MimicLateral(Modality):
+class MimicLateral(ModalityIMG):
     def __init__(self, enc, dec):
-        self.name = 'Lateral';
-        self.likelihood_name = 'laplace';
-        self.data_size = torch.Size((1, 128, 128));
-        self.gen_quality_eval = True;
-        self.file_suffix = '.png';
-        self.encoder = enc;
-        self.decoder = dec;
-        self.likelihood = mimic.modalities.utils.get_likelihood(self.likelihood_name);
-
-    def save_data(self, exp, d, fn, args):
-        img_per_row = args['img_per_row'];
-        write_samples_img_to_file(d, fn, img_per_row);
-
-    def plot_data(self, exp, d):
-        if not d.shape == self.data_size:
-            transform = transforms.Compose([transforms.ToPILImage(),
-                                            transforms.Resize(size=self.data_size[1:],
-                                                              interpolation=Image.BICUBIC),
-                                            transforms.ToTensor()])
-            d = transform(d.cpu())
-        p = d.repeat(1, 1, 1, 1);
-        return p;
+        self.name = 'Lateral'
+        self.likelihood_name = 'laplace'
+        self.data_size = torch.Size((1, 128, 128))
+        super().__init__(data_size=self.data_size)
+        self.gen_quality_eval = True
+        self.file_suffix = '.png'
+        self.encoder = enc
+        self.decoder = dec
+        self.likelihood = mimic.modalities.utils.get_likelihood(self.likelihood_name)
--- mimic/modalities/Modality.py
+++ mimic/modalities/Modality.py
@@ -1,5 +1,14 @@
 from abc import ABC, abstractmethod
 import torch
+import typing
+
+import PIL.Image as Image
+import torch
+from torch import Tensor
+from torchvision import transforms
+
+import mimic.modalities.utils
+from mimic.utils.save_samples import write_samples_img_to_file
 
 
 class Modality(ABC):
@@ -14,5 +23,22 @@ class Modality(ABC):
 
     def calc_log_prob(self, out_dist, target: torch.Tensor, norm_value: int):
         log_prob = out_dist.log_prob(target).sum()
-        mean_val_logprob = log_prob / norm_value
-        return mean_val_logprob
+        return log_prob / norm_value
+
+
+class ModalityIMG(Modality):
+    def __init__(self, data_size):
+        self.data_size = data_size
+
+    def save_data(self, exp, d, fn, args):
+        img_per_row = args['img_per_row']
+        write_samples_img_to_file(d, fn, img_per_row)
+
+    def plot_data(self, exp, d: Tensor):
+        if d.shape != self.data_size:
+            transform = transforms.Compose([transforms.ToPILImage(),
+                                            transforms.Grayscale(),
+                                            transforms.Resize(size=self.data_size[1:], interpolation=Image.BICUBIC),
+                                            transforms.ToTensor()])
+            d = transform(d.cpu())
+        return d.repeat(1, 1, 1, 1)
