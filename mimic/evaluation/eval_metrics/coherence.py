73f9eac7404581e1ef1e20742dd105ccf77c2248
klugh <klugh@student.ethz.ch>
2020-12-15 15:45:55 +0100

    computing eval metric over all batches instead of batchwise

--- mimic/evaluation/eval_metrics/coherence.py
+++ mimic/evaluation/eval_metrics/coherence.py
@@ -1,14 +1,18 @@
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from mimic.utils.exceptions import CudaOutOfMemory
+import typing
 from contextlib import contextmanager
 
-from mimic.utils.save_samples import save_generated_samples_singlegroup
-from mimic.utils.utils import at_most_n
+import numpy as np
+import torch
+from torch import Tensor
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
 from mimic import log
-import typing
+from mimic.utils.exceptions import CudaOutOfMemory
+from mimic.utils.save_samples import save_generated_samples_singlegroup
+from mimic.utils.utils import at_most_n, dict_to_device
+from itertools import product
+from typing import Mapping, Union
 
 
 @contextmanager
@@ -26,16 +30,17 @@ def catching_cuda_out_of_memory(batch_size):
             raise e
 
 
-def classify_cond_gen_samples(exp, labels, cond_samples):
+def classify_cond_gen_samples_(exp, labels: Tensor, cond_samples: typing.Mapping[str, Tensor]) \
+        -> typing.Mapping[str, typing.Mapping[str, float]]:
     labels = np.reshape(labels, (labels.shape[0], len(exp.labels)))
     clfs = exp.clfs
-    eval_labels = {l_key: {} for l, l_key in enumerate(exp.labels)}
+    eval_labels = {l_key: {} for l_key in exp.labels}
     for key in clfs.keys():
         if key in cond_samples.keys():
             mod_cond_gen = cond_samples[key]
             mod_clf = clfs[key]
             mod_cond_gen = transform_gen_samples(mod_cond_gen, exp.clf_transforms[key]).to(exp.flags.device)
-
+            # classify generated sample to evaluate coherence
             attr_hat = mod_clf(mod_cond_gen)
             for l, label_str in enumerate(exp.labels):
                 if exp.flags.dataset == 'testing':
@@ -44,12 +49,36 @@ def classify_cond_gen_samples(exp, labels, cond_samples):
                     score = exp.eval_label(np.nan_to_num(attr_hat.cpu().data.numpy()), labels,
                                            index=l)
                 else:
-                    score = exp.eval_label(attr_hat.cpu().data.numpy(), labels,
-                                           index=l)
+                    # score is nan if it only contains one class.
+                    score = exp.eval_label(attr_hat.cpu().data.numpy(), labels, index=l)
                 eval_labels[label_str][key] = score
         else:
             print(str(key) + 'not existing in cond_gen_samples')
     return eval_labels
+
+
+def classify_cond_gen_samples(exp, labels: Tensor, cond_samples: typing.Mapping[str, Tensor]) \
+        -> typing.Mapping[str, Tensor]:
+    """
+    Classifies for each modality all the conditionally generated samples.
+    Returns a dict like the following:
+    {'PA': tensor,
+    'Lateral': tensor,
+    'text': tensor}
+
+    """
+    clfs = exp.clfs
+    clf_predictions = {mod: {} for mod in exp.modalities}
+    for mod in exp.modalities:
+        if mod in cond_samples:
+            mod_cond_gen: Tensor = cond_samples[mod]
+            mod_clf = clfs[mod]
+            mod_cond_gen = transform_gen_samples(mod_cond_gen, exp.clf_transforms[mod]).to(exp.flags.device)
+            # classify generated sample to evaluate coherence
+            clf_predictions[mod] = mod_clf(mod_cond_gen).cpu()
+        else:
+            log.info(str(mod) + 'not existing in cond_gen_samples')
+    return clf_predictions
 
 
 def calculate_coherence(exp, samples) -> dict:
@@ -112,25 +141,64 @@ def save_generated_samples(exp, rand_gen: dict, iteration: int, batch_d: dict) -
                                            batch_d)
 
 
-def init_gen_perf(exp) -> typing.Mapping[str, dict]:
+def init_gen_perf(labels, subsets, mods) -> typing.Mapping[str, dict]:
     """
-    Initialises gen_perf dict with empty iterables
+    Initialises gen_perf dict with empty iterables.
+    The result will look like this:
+
+    {'cond':
+        {'Lung Opacity':
+            {'PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()}},
+      'Pleural Effusion':
+          {'PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()}},
+      'Support Devices':
+          {'PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()},
+           'Lateral_PA_text': {'PA': Tensor(), 'Lateral': Tensor(), 'text': Tensor()}}},
+
+    'random':
+        {'Lung Opacity': Tensor(), 'Pleural Effusion': Tensor(), 'Support Devices': Tensor()}}
     """
-    gen_perf = {'cond': dict(),
-                'random': dict()}
+    return {'cond': init_twolevel_nested_dict(labels, subsets, init_val={mod: torch.Tensor() for mod in mods}),
+            'random': {k: [] for k in labels}}
+
+
+def calc_coherence_random_gen(exp, mm_vae, iteration: int, gen_perf: typing.Mapping[str, dict], batch_d: dict) -> \
+        typing.Mapping[str, dict]:
+    args = exp.flags
+    # generating random samples
+    with catching_cuda_out_of_memory(batch_size=args.batch_size):
+        rand_gen = mm_vae.module.generate() if args.distributed else mm_vae.generate()
+    rand_gen = {k: v.to(args.device) for k, v in rand_gen.items()}
+    # classifying generated examples
+    coherence_random = calculate_coherence(exp, rand_gen)
     for j, l_key in enumerate(exp.labels):
-        gen_perf['cond'][l_key] = {}
-        for k, s_key in enumerate(exp.subsets.keys()):
-            if s_key != '':
-                gen_perf['cond'][l_key][s_key] = {}
-                for m, m_key in enumerate(exp.modalities.keys()):
-                    gen_perf['cond'][l_key][s_key][m_key] = []
-        gen_perf['random'][l_key] = []
+        gen_perf['random'][l_key].append(coherence_random[l_key])
+
+    if (exp.flags.batch_size * iteration) < exp.flags.num_samples_fid:
+        # saving generated samples to dir_fid
+        save_generated_samples(exp, rand_gen, iteration, batch_d)
 
     return gen_perf
 
 
-def test_generation(epoch, exp):
+def test_generation_(epoch, exp):
     args = exp.flags
 
     mods = exp.modalities
@@ -139,7 +207,7 @@ def test_generation(epoch, exp):
 
     gen_perf = init_gen_perf(exp)
     old_batch_size = args.batch_size
-    args.batch_size = 50
+    args.batch_size = 5
     log.info(f'setting batch size to {args.batch_size}')
     d_loader = DataLoader(exp.dataset_test,
                           batch_size=args.batch_size,
@@ -147,6 +215,8 @@ def test_generation(epoch, exp):
                           num_workers=exp.flags.dataloader_workers, drop_last=True)
 
     total_steps = None if args.steps_per_training_epoch < 0 else args.steps_per_training_epoch
+    # temp
+    total_steps = 1
     for iteration, batch in tqdm(enumerate(at_most_n(d_loader, total_steps)),
                                  total=len(d_loader), postfix='test_generation'):
         batch_d = batch[0]
@@ -200,3 +270,88 @@ def test_generation(epoch, exp):
 
     args.batch_size = old_batch_size
     return gen_perf
+
+
+def init_twolevel_nested_dict(level1_keys, level2_keys, init_val: any) -> dict:
+    """
+    Initialises a 2 level nested dict with value: init_val
+    HK, 15.12.20
+    """
+    return {l1: {l2: init_val for l2 in level2_keys if l2} for l1 in level1_keys if l1}
+
+
+def eval_classified_gen_samples(exp, subsets, mods, cond_gen_classified, gen_perf, batch_labels):
+    """
+    HK, 15.12.20
+    """
+    # compare the classification on the generated samples with the ground truth
+    for l_idx, l_key in enumerate(exp.labels):
+        for s_key, m_key in product(subsets, mods):
+            perf = exp.mean_eval_metric(
+                exp.eval_label(cond_gen_classified[s_key][m_key].cpu().data.numpy(), batch_labels, l_idx))
+            gen_perf['cond'][l_key][s_key][m_key] = perf
+            if np.isnan(perf):
+                log.warning(f'mean coherence eval metric "cond" is nan for cond {l_key},{s_key},{m_key}')
+                log.debug(
+                    f'mean coherence eval metric "cond" is nan for cond {l_key},{s_key},{m_key}. '
+                    f'With coherence gen perf:\n {gen_perf["cond"][l_key][s_key][m_key]}')
+        eval_score = exp.mean_eval_metric(gen_perf['random'][l_key])
+        if np.isnan(eval_score):
+            log.warning(f'mean coherence eval metric is nan for the label {l_key}')
+            log.debug(
+                f'mean coherence eval metric for the label {l_key} is nan. '
+                f'With coherence eval metric:\n {gen_perf["random"][l_key]}')
+        gen_perf['random'][l_key] = eval_score
+    return gen_perf
+
+
+def test_generation(epoch, exp):
+    """
+    Generates random and conditioned samples and evaluates coherence.
+    """
+    args = exp.flags
+
+    mods = exp.modalities
+    mm_vae = exp.mm_vae
+    subsets = exp.subsets
+    del subsets['']
+    labels = exp.labels
+
+    gen_perf = init_gen_perf(labels, subsets, mods)
+    d_loader = DataLoader(exp.dataset_test,
+                          batch_size=args.batch_size,
+                          shuffle=True,
+                          num_workers=exp.flags.dataloader_workers, drop_last=True)
+
+    total_steps = None if args.steps_per_training_epoch < 0 else args.steps_per_training_epoch
+    # all labels accumulated over batches:
+    batch_labels = torch.Tensor()
+    cond_gen_classified = init_twolevel_nested_dict(subsets, mods, init_val=torch.Tensor())
+    cond_gen_classified: Mapping[subsets, Mapping[mods, Tensor]]
+
+    for iteration, (batch_d, batch_l) in tqdm(enumerate(at_most_n(d_loader, total_steps)),
+                                              total=len(d_loader), postfix='test_generation'):
+
+        batch_labels = torch.cat((batch_labels, batch_l), 0)
+        batch_d = dict_to_device(batch_d, exp.flags.device)
+        # evaluating random generation
+        gen_perf = calc_coherence_random_gen(exp, mm_vae, iteration, gen_perf, batch_d)
+
+        # evaluating conditional generation
+        # first generates the conditional gen_samples
+        # classifies them and stores the classifier predictions
+        inferred = mm_vae.module.inference(batch_d) if args.distributed else mm_vae.inference(batch_d)
+        lr_subsets = inferred['subsets']
+        cg = mm_vae.module.cond_generation(lr_subsets) if args.distributed else mm_vae.cond_generation(lr_subsets)
+        cg: typing.Mapping[subsets, typing.Mapping[mods, Tensor]]
+
+        # classify the cond. generated samples
+        for cond_key, cond_val in cg.items():
+            clf_cg: Mapping[mods, Tensor] = classify_cond_gen_samples(exp, batch_l, cond_val)
+            for mod in mods:
+                cond_gen_classified[cond_key][mod] = torch.cat((cond_gen_classified[cond_key][mod], clf_cg[mod]), 0)
+            if (exp.flags.batch_size * iteration) < exp.flags.num_samples_fid:
+                save_generated_samples_singlegroup(exp, iteration, cond_key, cond_val)
+
+    gen_perf['cond']: typing.Mapping[str, typing.Mapping[str, typing.Mapping[str, float]]]
+    return eval_classified_gen_samples(exp, subsets, mods, cond_gen_classified, gen_perf, batch_labels)
