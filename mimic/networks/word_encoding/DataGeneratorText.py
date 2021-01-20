749c6ce739ea4f9522bdf525f3993d34faf5cf12
klugh <klugh@student.ethz.ch>
2021-01-20 15:48:17 +0100

    made number of reblocks dependent of parameter len_sequence

--- mimic/networks/word_encoding/DataGeneratorText.py
+++ mimic/networks/word_encoding/DataGeneratorText.py
@@ -15,10 +15,14 @@ def make_res_block_decoder(in_channels, out_channels, kernelsize, stride, paddin
                                                     dilation=dilation,
                                                     output_padding=o_padding),
                                  nn.BatchNorm1d(out_channels))
-    layers = []
-    layers.append(
+    layers = [
         ResidualBlock1dTransposeConv(in_channels, out_channels, kernelsize, stride, padding, dilation, o_padding,
-                                     upsample=upsample, a=a_val, b=b_val))
+                                     upsample=upsample,
+                                     a=a_val,
+                                     b=b_val,
+                                     )
+    ]
+
     return nn.Sequential(*layers)
 
 
@@ -26,39 +30,51 @@ class DataGeneratorText(nn.Module):
     def __init__(self, args, a=2.0, b=0.3):
         super(DataGeneratorText, self).__init__()
         self.args = args
-        self.resblock_1 = make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
-                                                 kernelsize=4, stride=1, padding=0, dilation=1, o_padding=0)
-        self.resblock_2 = make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_3 = make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_4 = make_res_block_decoder(5 * args.DIM_text, 4 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_5 = make_res_block_decoder(4 * args.DIM_text, 4 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_6 = make_res_block_decoder(4 * args.DIM_text, 3 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_7 = make_res_block_decoder(3 * args.DIM_text, 2 * args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        self.resblock_8 = make_res_block_decoder(2 * args.DIM_text, args.DIM_text,
-                                                 kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0)
-        # if the output dimension of the resblock_8 is bigger than the sequence length, a simple convolution is needed
-        # to reduce the output dim to the sequence length. Otherwise a transposed conv is used.
-        if self.args.len_sequence > 500:
-            self.conv2 = nn.ConvTranspose1d(args.DIM_text, self.args.vocab_size,
-                                            kernel_size=4,
-                                            stride=2,
-                                            padding=1,
-                                            dilation=1,
-                                            output_padding=0)
-        else:
-            self.conv2 = nn.Conv1d(self.args.DIM_text, self.args.vocab_size, stride=4, kernel_size=4)
+        modules = [make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
+                                          kernelsize=4, stride=1, padding=0, dilation=1, o_padding=0,
+                                          ),
+                   make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
+                                          kernelsize=4, stride=2, padding=1, dilation=1,
+                                          o_padding=0),
+                   make_res_block_decoder(5 * args.DIM_text, 5 * args.DIM_text,
+                                          kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0),
+                   make_res_block_decoder(5 * args.DIM_text, 4 * args.DIM_text,
+                                          kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0),
+                   make_res_block_decoder(4 * args.DIM_text, 4 * args.DIM_text,
+                                          kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0),
+                   ]
 
-        # inverts the 'embedding' module upto one-hotness
-        # needs input shape of (batch_size, self.args.DIM_text)
-        self.toVocabSize = nn.Linear(self.args.DIM_text, self.args.vocab_size)
+        if args.len_sequence >= 1024:
+            modules.append(make_res_block_decoder(4 * args.DIM_text, 3 * args.DIM_text,
+                                                  kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0))
+            modules.append(make_res_block_decoder(3 * args.DIM_text, 2 * args.DIM_text,
+                                                  kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0))
+            modules.append(make_res_block_decoder(2 * args.DIM_text, args.DIM_text,
+                                                  kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0))
+
+            modules.append(nn.ConvTranspose1d(args.DIM_text, self.args.vocab_size,
+                                              kernel_size=4,
+                                              stride=2,
+                                              padding=1,
+                                              dilation=1,
+                                              output_padding=0))
+        elif args.len_sequence == 128:
+            modules.append(make_res_block_decoder(4 * args.DIM_text, 1 * args.DIM_text,
+                                                  kernelsize=4, stride=2, padding=1, dilation=1, o_padding=0))
+            modules.append(nn.Conv1d(args.DIM_text, self.args.vocab_size,
+                                     kernel_size=1,
+                                     stride=1,
+                                     padding=0,
+                                     dilation=1))
+        else:
+            raise NotImplementedError(
+                f'The output shapes of this network will not work for len_sequence: {args.len_sequence}.'
+                f' Please adapt them.')
+
         self.softmax = nn.LogSoftmax(dim=1)
         self.sigmoid = nn.Sigmoid()
+        modules.append(self.sigmoid)
+        self.generator = nn.Sequential(*modules)
 
     def forward(self, feats):
         """
@@ -69,21 +85,10 @@ class DataGeneratorText(nn.Module):
         torch.Size([200, 512, 32])
         torch.Size([200, 512, 64])
         torch.Size([200, 384, 128])
-        torch.Size([200, 256, 256])
-        torch.Size([200, 128, 512])
+        if sequ_length > 128:
+            torch.Size([200, 256, 256])
+            torch.Size([200, 128, 512])
         conv2: torch.Size([bs, vocab_size, sequ_length])
         """
-        d = self.resblock_1(feats)
-        d = self.resblock_2(d)
-        d = self.resblock_3(d)
-        d = self.resblock_4(d)
-        d = self.resblock_5(d)
-        d = self.resblock_6(d)
-        d = self.resblock_7(d)
-        d = self.resblock_8(d)
-        d = self.conv2(d)
-        # d = d.view(-1, self.args.DIM_text)
-        # d = self.toVocabSize(d)
-        # d = self.softmax(d)
-        d = self.sigmoid(d)
-        return d
+
+        return self.generator(feats)
